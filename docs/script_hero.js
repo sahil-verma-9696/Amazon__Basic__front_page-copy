1d36e8777058a4ed3fd5edc9bbfca3f9cec243c4
sahil <sahils.verma.1000@gmail.com>
2025-03-03 19:42:33 +0530

    removing from desktop

--- docs/script_hero.js
+++ docs/script_hero.js
@@ -1,60 +1,65 @@
-function heroControls() {
-    const que = Array.from(document.querySelectorAll(".hero__slides"));
-    const leftbtn = document.querySelector(".hero__btn--left");
-    const rightbtn = document.querySelector(".hero__btn--right");
 
-    que[que.length - 1].classList.add("pre");
-    que[0].classList.add("cent");
-    que[1].classList.add("next");
+const que = Array.from(document.querySelectorAll(".hero__slides"));
+const leftbtn = document.querySelector(".hero__btn--left");
+const rightbtn = document.querySelector(".hero__btn--right");
 
-    leftbtn.addEventListener("click",right)
-    rightbtn.addEventListener("click",left)
+que[que.length - 1].classList.add("pre");
+que[0].classList.add("cent");
+que[1].classList.add("next");
 
-    function left() {
+leftbtn.addEventListener("click", right)
+rightbtn.addEventListener("click", left)
 
-        que[que.length - 1].classList.remove("pre");
+function left() {
 
-        que[0].classList.add("left");
-        que[1].classList.add("left");
-        setTimeout(() => {
-            que[0].classList.remove("cent");    
-            que[1].classList.remove("next");
+    que[que.length - 1].classList.remove("pre");
 
-            que.push(que.shift());
-
-            que[que.length - 1].classList.add("pre");
-            que[que.length - 1].classList.remove("left");
-
-            que[0].classList.add("cent");
-            que[0].classList.remove("left");
-
-            que[1].classList.add("next");
-
-        }, 50);
-
-    }
-    
-    function right() {
+    que[0].classList.add("left");
+    que[1].classList.add("left");
+    setTimeout(() => {
+        que[0].classList.remove("cent");
         que[1].classList.remove("next");
 
-        que[que.length - 1].classList.add("right");
-        que[0].classList.add("right");
-        setTimeout(() => {
-            que[0].classList.remove("cent");
-            que[que.length - 1].classList.remove("pre");
+        que.push(que.shift());
 
-            que.unshift(que.pop());
-            console.log(que)
-            que[1].classList.add("next");
-            que[1].classList.remove("right");
+        que[que.length - 1].classList.add("pre");
+        que[que.length - 1].classList.remove("left");
 
-            que[0].classList.add("cent");
-            que[0].classList.remove("right");
+        que[0].classList.add("cent");
+        que[0].classList.remove("left");
 
-            que[que.length - 1].classList.add("pre");
+        que[1].classList.add("next");
 
-        }, 50);
-    }
+    }, 10);
+
 }
 
-heroControls();
+function right() {
+    que[1].classList.remove("next");
+
+    que[que.length - 1].classList.add("right");
+    que[0].classList.add("right");
+    setTimeout(() => {
+        que[0].classList.remove("cent");
+        que[que.length - 1].classList.remove("pre");
+
+        que.unshift(que.pop());
+        console.log(que)
+        que[1].classList.add("next");
+        que[1].classList.remove("right");
+
+        que[0].classList.add("cent");
+        que[0].classList.remove("right");
+
+        que[que.length - 1].classList.add("pre");
+
+    }, 10);
+}
+
+
+setInterval(() => {
+    // left()
+    console.log("hi")
+}, 2000);
+
+
